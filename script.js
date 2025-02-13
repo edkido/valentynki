ce115c380f52fc51eeef02a7de4b773d1bc89875
Eduard Herasym <edkido29@gmail.com>
2025-02-13 15:21:06 +0100

    mobile

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const noButton = document.getElementById("no");
-const yesButton = document.getElementById("yes");
-
-function jumpingButton() {
-  const directions = [
-    { x: 60, y: 0 },
-    { x: -60, y: 0 },
-    { x: 0, y: 60 },
-    { x: 0, y: -60 },
-    { x: 60, y: 60 },
-    { x: -60, y: -60 },
-    { x: 60, y: -60 }, 
-    { x: -60, y: 60 }, 
-  ];
-  
-  const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-
-  const currentX = noButton.offsetLeft;
-  const currentY = noButton.offsetTop;
-  
-  const maxX = window.innerWidth - noButton.offsetWidth;
-  const maxY = window.innerHeight - noButton.offsetHeight;
-  
-  let newX = Math.max(0, Math.min(maxX, currentX + randomDirection.x));
-  let newY = Math.max(0, Math.min(maxY, currentY + randomDirection.y));
-
-  noButton.style.position = "absolute"; 
-  noButton.style.left = `${newX}px`;
-  noButton.style.top = `${newY}px`;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
   const card = document.querySelector(".card");
@@ -53,15 +22,4 @@ document.addEventListener("DOMContentLoaded", () => {
       card.style.transition = "top 0.5s";
       card.style.top = "0";
   }, true);
-
-  noButton.addEventListener("mouseover", () => jumpingButton());
-  // noButton.addEventListener("mouseenter", () => jumpingButton());
-  noButton.addEventListener("click", () => {
-    jumpingButton();
-    alert("но но но, містер Булочка")
-  });
-
-  yesButton.addEventListener("click", () => {
-    alert("Кохаю тебе, Булочка!");
-  });
 });
